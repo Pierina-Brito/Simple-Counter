cb7f7b78efd06d757e78db478d2aca52041ac66c
PIERINA BRITO <pierina.brito@tbiil.edu>
2023-03-14 02:15:38 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -15,12 +15,12 @@ function SimpleCounter(props) {
         <div className="calendar">
             <i className="fa fa-clock"></i>
         </div>
-        <div className="Six">{props.digitSix}</div>
-        <div className="Five">{props.digitFive}</div>
-        <div className="Four">{props.digitFour}</div>
-        <div className="Three">{props.digitThree}</div>
-        <div className="Two">{props.digitTwo}</div>
-        <div className="One">{props.digitOne}</div>
+        <div className="Six">{props.digitSix % 10}</div>
+        <div className="Five">{props.digitFive % 10}</div>
+        <div className="Four">{props.digitFour % 10}</div>
+        <div className="Three">{props.digitThree % 10}</div>
+        <div className="Two">{props.digitTwo % 10}</div>
+        <div className="One">{props.digitOne % 10}</div>
     </div>);
 
 }
